355da9d384d9053abd3c803863febe98dae1ff57
Deepak Mecheri <deepakmecheri@gmail.com>
2019-07-28 17:25:19 +0530

    Stage 12

---
+++ xfs-interface/load.sh
@@ -0,0 +1,5 @@
+load --os ../spl/spl_progs/os_startup.xsm
+load --init ../spl/spl_progs/print_num.xsm
+load --int=timer ../spl/spl_progs/timer.xsm
+load --int=7 ../spl/spl_progs/int7.xsm
+load --idle ../expl/expl_progs/idle.xsm
