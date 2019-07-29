43a4485ac7f8095ff2511b2d471c72e096aecba6
Deepak Mecheri <deepakmecheri@gmail.com>
2019-07-29 12:43:25 +0530

    Stage 13

--- xfs-interface/load.sh
+++ xfs-interface/load.sh
@@ -3,3 +3,4 @@ load --init ../spl/spl_progs/print_num.xsm
 load --int=timer ../spl/spl_progs/timer.xsm
 load --int=7 ../spl/spl_progs/int7.xsm
 load --idle ../expl/expl_progs/idle.xsm
+load --module 7 ../spl/spl_progs/mod7.xsm
