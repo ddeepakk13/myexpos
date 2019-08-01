62551796f2da24633292bd0b2b5d3e1020ddc066
Deepak Mecheri <deepakmecheri@gmail.com>
2019-08-01 21:45:26 +0530

    Stage 14

--- xfs-interface/load.sh
+++ xfs-interface/load.sh
@@ -1,6 +1,8 @@
 load --os ../spl/spl_progs/os_startup.xsm
-load --init ../spl/spl_progs/print_num.xsm
+load --init ../expl/expl_progs/odd.xsm  
+load --idle ../expl/expl_progs/idle.xsm
 load --int=timer ../spl/spl_progs/timer.xsm
 load --int=7 ../spl/spl_progs/int7.xsm
-load --idle ../expl/expl_progs/idle.xsm
-load --module 7 ../spl/spl_progs/mod7.xsm
+load --int=10 ../spl/spl_progs/int10.xsm
+load --module 5 ../spl/spl_progs/scheduler.xsm
+load --module 7 ../spl/spl_progs/boot_module.xsm
