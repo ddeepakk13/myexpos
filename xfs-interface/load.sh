ecb4d467e39958a020f0ad6033f36fe89f94905a
Deepak Mecheri <deepakmecheri@gmail.com>
2019-10-31 21:22:11 +0530

    Stage 21

--- xfs-interface/load.sh
+++ xfs-interface/load.sh
@@ -1,13 +1,16 @@
 load --os ../spl/spl_progs/os_startup.xsm
-load --init ../expl/expl_progs/program_prompt.xsm
+load --init ../expl/expl_progs/shell2.xsm
 load --idle ../expl/expl_progs/idle.xsm
 load --int=timer ../spl/spl_progs/timer.xsm
 load --int=disk ../spl/spl_progs/disk.xsm
 load --int=console ../spl/spl_progs/console.xsm
 load --int=6 ../spl/spl_progs/int6.xsm
 load --int=7 ../spl/spl_progs/int7.xsm
+load --int=8 ../spl/spl_progs/int8.xsm
 load --int=9 ../spl/spl_progs/int9.xsm
 load --int=10 ../spl/spl_progs/int10.xsm
+load --int=11 ../spl/spl_progs/int11.xsm
+load --int=15 ../spl/spl_progs/int15.xsm
 load --exhandler ../spl/spl_progs/exception.xsm
 load --module 0 ../spl/spl_progs/resource_manager.xsm
 load --module 1 ../spl/spl_progs/process_manager.xsm
